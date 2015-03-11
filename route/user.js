591e8aac107e7bb023bca345229b48ca4b3e1a90
sunxiaoshen <sunxiaoshen@meituan.com>
2015-03-11 16:20:01 +0800

    fix

--- route/user.js
+++ route/user.js
@@ -7,7 +7,7 @@ router.post('/auth', function(req, res) {
     var data = req.body;
     User.find({name: data.name, pwd: data.pwd}, function(err, user) {
         if (err) throw err;
-        if (user) {
+        if (user.name == data.name) {
             res.status(200).json({status: 200, data: user});
         } else {
             res.status(200).json({status: 201, msg: '用户名密码错误'});
@@ -17,6 +17,7 @@ router.post('/auth', function(req, res) {
 
 router.post('/save', function(req, res) {
     var data = req.body;
+    console.log(data);
     if (data.pwd != data.pwd2) {
         res.status(200).json({status: 201, msg: '两次输入密码不匹配'});
     } else {
